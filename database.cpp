894b48ae83512277eb6beb63ee3ed442ebca196f
Jerry Groom <Jerry.groom@gmail.com>
2015-04-07 17:09:59 -0500

    Completed Individual Project
    Database.cpp is complete and ready to be graded

--- database.cpp
+++ database.cpp
@@ -1,8 +1,18 @@
-//--------------------------------------------------------------------
-//
-//  Laboratory 9, ProgrammingExercise 1                    database.cpp
-//
-//--------------------------------------------------------------------
+/*
+*******************************************************************
+
+Laboratory 9
+Binary Search Tree || Individual Project
+
+Jerry Groom
+4/07/2015
+
+*******************************************************************
+*/
+
+
+
+
 
 #include <iostream>
 #include <fstream>
@@ -11,10 +21,19 @@
 using namespace std;
 
 
+
+
+
+
 const int nameLength      = 11;   // Maximum number of characters in a name
-const long bytesPerRecord = 37;   // Number of bytes used to store each record in the accounts database file
+
+//const long bytesPerRecord = 37;   // Number of bytes used to store each record in the accounts database file
+const long bytesPerRecord = 38;   // <-- It only works when I use 38 rather than 37, Im
+// not quite certain why, but it does.
+
 
 // struct that holds account information
+
 struct AccountRecord
 {
     int acctID;                   // Account identifier
@@ -24,13 +43,19 @@ struct AccountRecord
 };
 
 // struct that holds na entry in the database index
+
+
+
+
 struct IndexEntry
 {
     int acctID;              // (Key) Account identifier
     long recNum;             // Record number
 
     int getKey () const
-        { return acctID; }   // Return key field
+    {
+        return acctID;    // Return key field
+    }
 };
 
 
@@ -39,49 +64,56 @@ int main ()
 {
     ifstream acctFile ("accounts.dat");   // Accounts database file
     AccountRecord acctRec;                // Account record
-                                          // Database index   // <---------------------- add a line of code here
+    BSTree<IndexEntry,int> index;         // Database index   // <---------------------- add a line of code here
     IndexEntry entry;                     // Index entry
     int searchID;                         // User input account ID
     long recNum;                          // Record number
 
-	
+
     // Iterate through the database records. For each record, read the
     // account ID and add the (account ID, record number) pair to the index
+
     recNum = 0;
     while ( acctFile >> entry.acctID ) // puts the account ID into the entry
     {
-        entry.recNum = recNum; 
-        recNum++;  
-		              // <-------------------------------------------------------------- add a line of code here
+        entry.recNum = recNum;
+        recNum++;
+        index.insert(entry);     // <-------- add a line of code here
         acctFile.seekg(recNum*bytesPerRecord); // go to beginning of the line that contains the recNum record
     }
 
-	
-    // Output the account IDs in ascending order.
-    cout << endl << "Account IDs : " << endl;
-                     // <--------------------------------------------------------------- add a line of code here
 
-	
+
+// Output the account IDs in ascending order.
+    cout << endl << "Account IDs : " << endl;
+    index.writeKeys();    // <------------ add a line of code here
+
+
     // Clear the status flags for the database file.
     acctFile.clear();
 
-	
-    // Read an account ID from the keyboard and output the
+
+
+// Read an account ID from the keyboard and output the
     // corresponding record.
     cout << endl << "Enter account ID : ";
-    while (cin >> searchID) {
-        if (               ) {  // <---------------------------------------------------- fill in the if condition here
+    while (cin >> searchID)
+    {
+        if (index.retrieve(searchID, entry))    // <------- fill in the if condition here
+        {
             acctFile.seekg(entry.recNum*bytesPerRecord); // go to the beginning of the line that corresponds with the recNum record
             acctFile >> acctRec.acctID >> acctRec.firstName
                      >> acctRec.lastName >> acctRec.balance;
             cout << entry.recNum << " : " << acctRec.acctID << " "
                  << acctRec.firstName << " " << acctRec.lastName << " "
                  << acctRec.balance << endl;
-         } else {
-             cout << "No record with that account ID" << endl;
-	 }
+        }
+        else
+        {
+            cout << "No record with that account ID" << endl;
+        }
 
-	 cout << "Enter account ID (EOF to quit): ";
+        cout << "Enter account ID (EOF to quit): ";
     }
 
     cout << endl;
